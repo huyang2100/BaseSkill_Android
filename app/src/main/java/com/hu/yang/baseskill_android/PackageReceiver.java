f26553779f99f253bb1d3be6c51114550e027437
huyang2100 <huyang2100@163.com>
2017-07-12 23:02:03 +0800

    add package receiver

---
+++ app/src/main/java/com/hu/yang/baseskill_android/PackageReceiver.java
@@ -0,0 +1,10 @@
+package com.hu.yang.baseskill_android;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by Yang on 2017/7/12.
+ */
+
+public class PackageReceiver extends BroadcastReceiver {
+}
